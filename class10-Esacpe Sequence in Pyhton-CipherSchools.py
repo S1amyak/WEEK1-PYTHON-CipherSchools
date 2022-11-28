2bc4edd41009c2dea41dddee3d6dca1645e8110e
SAMYAK JAIN <samyak6117@gmail.com>
2022-11-28 23:37:37 +0530

    week1

---
+++ class10-Esacpe Sequence in Pyhton-CipherSchools.py
@@ -0,0 +1,4 @@
+print("line A\nline B \n lineC")
+print("name\tSamyak")
+print("this is double backslash\\\\")
+print("hell\blo")
---
+++ class11-Comments in Python-CipherSchools.py
@@ -0,0 +1,8 @@
+#this is comment
+print("hello A\n B\n lineC")#\n is for new line
+#\t is for tab
+print("name \tSamyak")
+print("this is double backslash\\\\") #\\is for backslash
+print("hell\blo")
+#comments
+#print("")
